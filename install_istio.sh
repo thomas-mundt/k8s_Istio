8a0e079305b7ab5142491cb5bdd25250f53ce6ea
Thomas Mundt <mundt_thomas@yahoo.de>
2021-08-05 15:18:04 +0200

    Create install_istio.sh

---
+++ install_istio.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.48.1/deploy/static/provider/do/deploy.yaml --kubeconfig=/Users/bdk/.kube/k8s-cluster.yaml
+
+curl -L https://istio.io/downloadIstio | sh -
+cd istio-1.10.3
+
+kubectl create namespace istio-system --kubeconfig=/Users/bdk/.kube/k8s-cluster.yaml
+helm install istio-base manifests/charts/base -n istio-system --kubeconfig=/Users/bdk/.kube/k8s-cluster.yaml
+helm install istiod manifests/charts/istio-control/istio-discovery -n istio-system --kubeconfig=/Users/bdk/.kube/k8s-cluster.yaml
+helm install istio-ingress manifests/charts/gateways/istio-ingress -n istio-system --kubeconfig=/Users/bdk/.kube/k8s-cluster.yaml
+helm install istio-egress manifests/charts/gateways/istio-egress -n istio-system --kubeconfig=/Users/bdk/.kube/k8s-cluster.yaml
+
+kubectl get pods -n istio-system --kubeconfig=/Users/bdk/.kube/k8s-cluster.yaml -w
